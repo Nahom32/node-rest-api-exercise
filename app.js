d22a0440b54803c00f70b9018265040bb90400bb
Nahom32 <senaynahom00@gmail.com>
2022-11-19 03:26:17 +0300

    created routes

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./api/routes/products.js')
 
-app.use((req,res,next)=>{
-    res.status(200).json({
-        message: 'It works!'
-    });
 
-});
+app.use('/product', productRoutes);
 
 module.exports = app;
