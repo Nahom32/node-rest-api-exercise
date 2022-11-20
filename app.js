755c138cc28061c0a23a070b2055250ef86a5ad3
Nahom32 <senaynahom00@gmail.com>
2022-11-20 14:05:11 +0300

    modified the error response

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 app.use((req,res,next)=>{
     const error = new Error('404 not Found');
-    error.status(404);
+    error.status = 404;
     next(error);
 });
 app.use((error,req,res,next)=>{
     res.status(error.status || 500);
     res.json({
+        error:{
         message: error.message || 'Internal Server Error',
-    })
+    }})
 })
 module.exports = app;
