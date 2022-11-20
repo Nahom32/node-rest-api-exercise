68c2122def994165fa5f7db63b3bbadd5ae501ef
Nahom32 <senaynahom00@gmail.com>
2022-11-20 04:40:31 +0300

    Handled Error methods

---
+++ api/routes/orders.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:productId',(req,res,next) => {
+    res.status(200).json({
+        message: 'Orders were fetched',
+        id: req.params.productId
+    })
+});
+
+router.post('/:productId',function(req,res,next){
+    res.status(201).json({
+        message: 'Orders were created',
+        id: req.params.productId
+    })
+});
+module.exports = router;  
+
+
--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-const productRoutes = require('./api/routes/products.js')
-
+const morgan = require('morgan');
+const productRoutes = require('./api/routes/products.js');
+const orderRoutes = require('./api/routes/orders.js');
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
-
+app.use('/orders', orderRoutes);
+app.use((req,res,next)=>{
+    const error = new Error('404 not Found');
+    error.status(404);
+    next(error);
+});
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        message: error.message || 'Internal Server Error',
+    })
+})
 module.exports = app;
